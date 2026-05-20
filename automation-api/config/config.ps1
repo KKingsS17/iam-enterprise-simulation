b687661f85377281dcf7ba3340e22b790bbd755a
Kendall Reyes <140760783+KKingsS17@users.noreply.github.com>
2026-05-20 17:32:18 -0600

    Add files via upload

--- automation-api/config/config.ps1
+++ automation-api/config/config.ps1
@@ -1,1 +1,8 @@
+$global:GraphConfig = @{
 
+    ClientId = "57d3042f-3c3f-4f99-b019-d277e8271709"
+
+    TenantId = "e5835762-a798-4d80-9355-5d5e64e49034"
+
+    CertificateThumbprint = "41C8F864928E3215D62C155BB2DB6BF1E23CD3FA"
+}
